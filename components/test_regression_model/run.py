9b35f53d3e5bed07b6d5d52ccb01529c67b4e166
NONTAWAT149 <nontawat149@gmail.com>
2022-07-10 21:10:41 +0100

    best hyper parameters

--- components/test_regression_model/run.py
+++ components/test_regression_model/run.py
@@ -35,10 +35,24 @@ def go(args):
 
     logger.info("Loading model and performing inference on test set")
     sk_pipe = mlflow.sklearn.load_model(model_local_path)
-    y_pred = sk_pipe.predict(X_test)
+
+    # add selected features
+    processed_feature = ["room_type",
+                        "neighbourhood_group",
+                         "minimum_nights",
+                         "number_of_reviews",
+                         "reviews_per_month",
+                         "calculated_host_listings_count",
+                         "availability_365",
+                         "longitude",
+                         "latitude",
+                         "last_review",
+                         "name"
+                        ]
+    y_pred = sk_pipe.predict(X_test[processed_feature])
 
     logger.info("Scoring")
-    r_squared = sk_pipe.score(X_test, y_test)
+    r_squared = sk_pipe.score(X_test[processed_feature], y_test)
 
     mae = mean_absolute_error(y_test, y_pred)
 
